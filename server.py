da3e8f3868e42ec4d3b3fbcd28ed51e1e693f23c
Kunal Gupta <kunallguptaa@gmail.com>
2019-04-28 21:34:01 +0530

    Made the implementation visible to public devices on the network

--- server.py
+++ server.py
@@ -59,6 +59,7 @@ def get_key(my_dict, val):
          if val == value: 
              return key 
 
+#failed test cases
 def verify_fails(srcName, destName):
 	global errStr
 	if srcName == '' and destName == '':
@@ -79,4 +80,4 @@ def verify_fails(srcName, destName):
 		return False, False
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
